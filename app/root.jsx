04ebc1d945add0d8670769f990830ab28dd8e74c
KhanMuhamamd <119910838+KhanMuhamamd@users.noreply.github.com>
2023-04-10 02:00:44 +0500

    allowing insecure requests

--- app/root.jsx
+++ app/root.jsx
@@ -28,6 +28,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <StylesPlaceholder />
         <Meta />
         <Links />
